d14c58bddf24bb4e624fa07aaef26c78fa4042a4
ManZun93 <manuellufan19@gmail.com>
2023-02-16 23:45:14 -0500

    test: nuevo dev

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout.jsx';
 export default function SignUp() {
   return (
     <>
-      <Layout>soy sign</Layout>
+      <Layout>registrate</Layout>
     </>
   );
 }
