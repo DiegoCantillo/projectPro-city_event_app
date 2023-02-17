ef009df9f4fe08531b24370dabe5e84f8a4e53e6
ManZun93 <manuellufan19@gmail.com>
2023-02-17 00:03:14 -0500

    style: agregando colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -16,6 +10,16 @@ module.exports = {
     extend: {
       colors: {
         'my-custom-color': '#fffff',
+        blue: '#1B4DB1',
+        yellow: '#F3F243',
+        pink: '#FF64BC',
+        black: '#000000',
+        lightBlack: '#1A1E2E',
+        darkGray: '#6E6A6C',
+        gray: '#A7A6A7',
+        lightGray: '#D9D9D9',
+        lighterGray: '#F8F7FA',
+        red: '#EF3F47',
       },
       screens: {
         xs: '375px',
